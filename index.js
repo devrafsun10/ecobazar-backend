c737670bd0dd2021b538e9c675d8adb5f3e09aca
devrafsun10 <ssiyam152@gmail.com>
2026-06-09 03:03:08 +0600

    multer setup

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const User = require('./models/userModels');
 const app = express();
 const { rateLimit } = require('express-rate-limit');
 const { getAllUsersController, singleUserDataController, deleteUserController, updateUserController } = require("./controllers/userControllers");
+const { createProductController, getProductController, getSingleProductController, productDeleteController, productUpdateController } = require("./controllers/productController");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/products');//set the destination for uploaded files
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);//set the filename for uploded files
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, 
@@ -34,7 +47,11 @@ app.post('/resendverificationemail', resendVerificationController )
 app.post('/verifyemail/:token', verifyEmailController)
 
 //product create
-app.post('/createproduct',)
+app.post('/createproduct',  upload.array('photos', 5) , createProductController)
+app.get('/allproduct', getProductController)
+app.get('/singleproduct/:id', getSingleProductController)
+app.get('/deleteproduct/:id', productDeleteController)
+app.put('/updateproduct/:id', productUpdateController)
 
 //order management
 
@@ -42,7 +59,7 @@ app.post('/createproduct',)
 app.get('/allusers', getAllUsersController)
 app.get('/singleuser/:id',singleUserDataController)
 app.delete('/deleteuser/:id',deleteUserController)
-app.post('/updateuser/:id',updateUserController)
+app.post('/updateuser/:id',upload.array('photos', 5),updateUserController)
 
 let port = process.env.PORT || 5000;
 
